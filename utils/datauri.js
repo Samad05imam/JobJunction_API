624ec3f211fbe3868fd54072a496fb6de1571dc5
Samad05imam <samad1419imam@gmail.com>
2025-07-01 20:40:16 +0530

    3rd

--- utils/datauri.js
+++ utils/datauri.js
@@ -2,10 +2,12 @@ import DataUriParser from "datauri/parser.js"
 
 import path from "path";
 
-const getDataUri = (file) => {
-    const parser = new DataUriParser();
-    const extName = path.extname(file.originalname).toString();
-    return parser.format(extName, file.buffer);
-}
+export const getDataUri = (file) => {
+  if (!file) throw new Error("No file provided to getDataUri");
+
+  const extName = path.extname(file.originalname);
+  return `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
+};
+
 
 export default getDataUri;
