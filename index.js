b9c0971ba10c6e4aef882a442f9587ca24000cb9
Samad05imam <samad1419imam@gmail.com>
2025-07-01 21:01:25 +0530

    fourth time

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
 const corsOption = {
-    origin : "http://localhost:5173",
+    origin : "https://job-junction-gray.vercel.app/",
     credentials : true
 }
 app.use(cors(corsOption));
